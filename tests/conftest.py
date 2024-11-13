725f968d05636f7d66166af3155ce0752cff0c46
adnanzai <adnanzai@umich.edu>
2024-11-12 22:33:12 -0500

    updates

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,26 +9,26 @@ def pytest_addoption(parser):
     parser.addoption(
         "--image-dir",
         action="store",
-        default=None,
+        default='sample',
         help="Path to the directory containing sample images and labels."
     )
     parser.addoption(
         "--image-size",
         action="store",
-        default=None,
+        default="128,128,128",
         help="Image size for transformations (e.g., '128,128,64')."
     )
     parser.addoption(
         "--roi-size",
         action="store",
-        default=None,
+        default="64,64,64",
         help="ROI size for transformations (e.g., '64,64,32')."
     )
 
 @pytest.fixture
 def get_data(pytestconfig):
     """Fixture to load all images and labels from the specified directory."""
-    data_dir = pytestconfig.getoption("image_dir")
+    data_dir = pytestconfig.getoption("--image-dir")
     if not data_dir or not os.path.isdir(data_dir):
         pytest.fail(f"The directory '{data_dir}' does not exist. Specify it using --image-dir.")
         
@@ -45,7 +45,7 @@ def get_data(pytestconfig):
         if image_file not in labelsTr_files:
             pytest.fail(f"No matching label found for the image '{image_file}' in 'labelsTr' folder.")
 
-    # Get list of all training images, and their labels
+    # Get list of all training images and their labels
     train_images = sorted(glob.glob(os.path.join(data_dir, "imagesTr", "*.nii.gz")))
     train_labels = sorted(glob.glob(os.path.join(data_dir, "labelsTr", "*.nii.gz")))
     data_dicts = [{"image": image_name, "label": label_name} for image_name, label_name in zip(train_images, train_labels)]
@@ -54,15 +54,15 @@ def get_data(pytestconfig):
 @pytest.fixture
 def transform_pipeline(pytestconfig):
     """Initialize the transformation pipeline with the imported image_size and roi_size."""
-    image_size = tuple(map(int, pytestconfig.getoption("image_size").split(',')))
-    roi_size = tuple(map(int, pytestconfig.getoption("roi_size").split(',')))
+    image_size = tuple(map(int, pytestconfig.getoption("--image-size").split(',')))
+    roi_size = tuple(map(int, pytestconfig.getoption("--roi-size").split(',')))
     train_transforms, _ = mtrain_transforms(image_size, roi_size=roi_size)
     return train_transforms
 
 @pytest.fixture
 def validation_transform_pipeline(pytestconfig):
     """Initialize the transformation pipeline with the imported image_size and roi_size."""
-    image_size = tuple(map(int, pytestconfig.getoption("image_size").split(',')))
-    roi_size = tuple(map(int, pytestconfig.getoption("roi_size").split(',')))
+    image_size = tuple(map(int, pytestconfig.getoption("--image-size").split(',')))
+    roi_size = tuple(map(int, pytestconfig.getoption("--roi-size").split(',')))
     _, val_transforms = mtrain_transforms(image_size, roi_size=roi_size)
     return val_transforms
